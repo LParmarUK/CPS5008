b4fd674b1edd372a12210aa81d2c5433eddb8e83
LParmarUK <LeonparmarUK@hotmail.com>
2026-04-30 20:39:32 +0100

    Data cleaning step including column formatting, ID removal

--- Churn_Project/src/main.py
+++ Churn_Project/src/main.py
@@ -2,8 +2,22 @@ from data_loader import load_data, basic_overview
 from eda import save_missing_values_chart, save_target_chart, detect_possible_leakage_columns
 
 
+def clean_data(df):
+    df.columns = df.columns.str.strip()
+
+    if "Customer ID" in df.columns:
+        df = df.drop(columns=["Customer ID"])
+
+    if df["Churn"].dtype == "object":
+        df["Churn"] = df["Churn"].map({"Yes": 1, "No": 0})
+
+    return df
+
+
 def main():
     df = load_data()
+    df = clean_data(df)
+
     basic_overview(df)
 
     save_missing_values_chart(df)
